7af0bb60da97b2c954b06de44c66e0d720d43a67
Gabriel Cabral <G.Cabral>
2020-09-17 13:45:30 -0300

    Adiciona a reposta do exercícos

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Cidades do litoral sul de São Paulo!");
+            Console.WriteLine();
+            Console.WriteLine("Bertioga");
+            Console.WriteLine("Cananéia");
+            Console.WriteLine("Cubatão");
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Ilha Comprida");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Santos");
+            Console.WriteLine("São Vicente");
+
+
+
+
+        }  
+    }
+}
